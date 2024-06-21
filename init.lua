03106b2736738af77fcac5b9f6d91730c17f35d9
Zack Peebles <77955661+zackpeebles@users.noreply.github.com>
2024-06-21 01:04:21 -0700

    moved harpoon config into lazy.nvim config table

--- init.lua
+++ init.lua
@@ -22,7 +22,5 @@ require 'lazy-bootstrap'
 
 require 'lazy-plugins'
 
-require 'init'
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,5 +3,54 @@ return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    event = 'VeryLazy',
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+      -- REQUIRED
+      --
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = '[A]dd file to Harpoon' })
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon quick menu' })
+      vim.keymap.set('n', '<leader>e', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon quick menu' })
+
+      vim.keymap.set('n', '<C-1>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-2>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-3>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-4>', function()
+        harpoon:list():select(4)
+      end)
+      vim.keymap.set('n', '<C-5>', function()
+        harpoon:list():select(5)
+      end)
+      vim.keymap.set('n', '<C-6>', function()
+        harpoon:list():select(6)
+      end)
+      vim.keymap.set('n', '<C-7>', function()
+        harpoon:list():select(7)
+      end)
+      vim.keymap.set('n', '<C-8>', function()
+        harpoon:list():select(8)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<M-p>', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<M-n>', function()
+        harpoon:list():next()
+      end)
+    end,
   },
 }
