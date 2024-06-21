f58c8f6a1e0c9b438a36a4fafbe89129995cee0a
Zack Peebles <77955661+zackpeebles@users.noreply.github.com>
2024-06-21 01:05:00 -0700

    configured lazy loading for faster startup

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,12 +1,13 @@
 -- [[ Configure and install plugins ]]
---
 --  To check the current status of your plugins, run
 --    :Lazy
-
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  --
+  { -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
+    event = 'VimEnter',
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -14,9 +15,9 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --  This is equivalent to:
   --    require('Comment').setup({})
-
   { -- "gc" to comment visual regions/lines
     'numToStr/Comment.nvim',
+    event = 'VeryLazy',
     opts = {
       extra = {
         eol = 'gca',
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,6 +6,7 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    event = { 'VeryLazy' },
     opts = {
       signs = {
         add = { text = '+' },
