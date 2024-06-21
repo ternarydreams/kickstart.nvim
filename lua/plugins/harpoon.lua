9fe1e1cd7d8ea4b422dcdca240296b12ddad5390
Zack Peebles <77955661+zackpeebles@users.noreply.github.com>
2024-06-21 00:17:07 -0700

    Optimized dependency of Harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,6 +2,6 @@ return {
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
 }
