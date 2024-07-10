7a5f1452389be40493dc8308ad74e176d41a779d
Zack Peebles <zackcpeebles@gmail.com>
2024-07-10 13:35:52 -0700

    removed conflict with ctrl-e

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,7 +12,7 @@ return {
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
       end, { desc = '[A]dd file to Harpoon' })
-      vim.keymap.set('n', '<C-e>', function()
+      vim.keymap.set('n', '<M-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = 'Harpoon quick menu' })
       -- vim.keymap.set('n', '<leader>e', function()
